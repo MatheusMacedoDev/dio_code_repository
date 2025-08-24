d0a32229878115eb8fbd3bb51f4af28a60b657c1
Matheus Macedo <matheus.macedo.developer@gmail.com>
2025-08-24 16:04:43 -0300

    feat: upload the sudoku game

---
+++ sudoku/src/main/java/org/matheus/models/GameStatusEnum.java
@@ -0,0 +1,17 @@
+package org.matheus.models;
+
+import lombok.Getter;
+
+@Getter
+public enum GameStatusEnum {
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private final String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+}
